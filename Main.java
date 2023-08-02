156ef9fc4fdba8dd6fb454a3ad113274a870d033
ErickIntegra <steezy7jefe@gmail.com>
2023-08-02 18:48:47 -0400

    version1.1

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("my first Upload");
+        System.out.println("so you come here often?");
+        System.out.println("what ur name toots");
+
+    }}
